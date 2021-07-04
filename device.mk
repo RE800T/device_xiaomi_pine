0c753d125968b7de379d0540e5348c59de130e4a
RE800T <brutalman122@gmail.com>
2021-07-04 17:54:56 +0000

    pine: Enable iorapd tracing with perfetto
    App startups will now have perfetto-based tracing from iorapd.
    
    Signed-off-by: RE800T <brutalman122@gmail.com>
    
    author: iam

--- device.mk
+++ device.mk
@@ -194,7 +194,8 @@ PRODUCT_PACKAGES += \
 
 # Iorapd
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.iorapd.enable=true
+    ro.iorapd.enable=true \
+    iorapd.perfetto.enable=true
 
 # IRQ
 PRODUCT_COPY_FILES += \
