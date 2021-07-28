6e3850d16331f49a75cc4d5adbd91a8c7179de69
RE800T <brutalman122@gmail.com>
2021-07-28 06:46:47 +0000

    Redfin july fp switch to coral july fp

--- lineage_pine.mk
+++ lineage_pine.mk
@@ -38,8 +38,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ3A.210605.005 7349499 release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RQ2A.210405.005/7181113:user/release-keys
+BUILD_DESCRIPTION := coral-user 11 RQ2A.210405.005 7181113 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
